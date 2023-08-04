ff1edc6fc7d529e1e04ad896a83ea4b11864d57d
Erin <github@eritbh.me>
2023-08-04 03:20:55 -0400

    make pagerForItems actually usable

--- extension/data/components/pager/PagerForItems.svelte
+++ extension/data/components/pager/PagerForItems.svelte
@@ -1,29 +1,16 @@
 <script lang="ts" generics="Item">
 	import Pager from './Pager.svelte';
 
-	type ContentFunction = (item: Item, index: number) => string;
-
 	export let items: Item[] = [];
 	export let itemsPerPage: number;
-	export let wrapper = 'div';
-	export let contentFunction: ContentFunction | null = null;
 </script>
 
 <Pager
 	pageCount={Math.ceil(items.length / itemsPerPage)}
 	let:pageIndex
 >
-	<svelte:element this={wrapper}>
-		{#each items.slice(
-			pageIndex * itemsPerPage,
-			(1 + pageIndex) * itemsPerPage
-		) as item, indexInPage}
-			{@const i = (pageIndex * itemsPerPage) + indexInPage}
-			<slot {item} {i}>
-				{#if contentFunction}
-					{@html contentFunction(item, i)}
-				{/if}
-			</slot>
-		{/each}
-	</svelte:element>
+	<slot pageItems={items.slice(
+		pageIndex * itemsPerPage,
+		(1 + pageIndex) * itemsPerPage
+	)} />
 </Pager>
