9b1c518fa998eb5a007bab27b9402b4a356f55bb
Erin <github@eritbh.me>
2023-08-04 04:36:52 -0400

    add RelativeTime component

---
+++ extension/data/components/time/RelativeTime.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import jQuery from 'jquery';
+
+	export let date: Date;
+
+	function timeago(el: HTMLTimeElement) {
+		// @ts-expect-error timeago is a jquery plugin with no types
+		jQuery(el).timeago();
+	}
+</script>
+
+<time datetime={date.toISOString()} use:timeago>
+	{date.toLocaleString()}
+</time>
